c5bc65fb004c3e4f85fd407c04b6cd52c0f9397f
gurkiratz <singh.gurkirat2707@gmail.com>
2025-04-17 15:36:56 -0400

    add custom styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,24 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Progress } from '@/components/ui/progress'
+import { Textarea } from '@/components/ui/textarea'
 import Image from 'next/image'
 
 export default function Home() {
-  return <div className="text-rose-500">Hello world</div>
+  return (
+    <div className="flex flex-col gap-y-4 p-8">
+      <div>
+        <Button variant="elevated">I am a button</Button>
+      </div>
+      <div>
+        <Input placeholder="I am an input" />
+      </div>
+      <div>
+        <Progress value={50} className="w-[400px]" />
+      </div>
+      <div>
+        <Textarea placeholder="I am textarea" />
+      </div>
+    </div>
+  )
 }
