c324521b4f7598ae1b19c5ac725d68de7783306a
gurkiratz <singh.gurkirat2707@gmail.com>
2025-04-18 18:37:21 -0400

    add home layout

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,11 +2,10 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Progress } from '@/components/ui/progress'
 import { Textarea } from '@/components/ui/textarea'
-import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-y-4 p-8">
+    <div className="flex flex-col gap-y-4 p-4">
       <div>
         <Button variant="elevated">I am a button</Button>
       </div>
